193e37d919762532082303bd5d0f618b3115aa47
harpap <har.pap@hotmail.com>
2018-06-27 13:02:52 +0300

    eisagwgi
    kala ws edw

--- src/compare.py
+++ src/compare.py
@@ -48,7 +48,7 @@ def main(args):
     output_dir_vid = os.path.expanduser(args.output_dir + '/video')
     if not os.path.exists(output_dir_vid):
         os.makedirs(output_dir_vid)
-    #frameGetter('C:/Users/computer science/Downloads/MasterChefGR 2.mp4',output_dir_vid)
+    #frame_getter('C:/Users/computer science/Downloads/MasterChefGR 2.mp4',output_dir_vid)
     dataset = facenet.get_dataset(args.output_dir)
     images = load_and_align_data(dataset[0].image_paths, args.image_size, args.margin, args.gpu_memory_fraction)
     with tf.Graph().as_default():
@@ -68,154 +68,45 @@ def main(args):
             emb = sess.run(embeddings, feed_dict=feed_dict)
 
             nrof_images = len(dataset[0].image_paths)
-            '''
-            print('Images:')
-            for i in range(nrof_images):
-                print('%1d: %s' % (i, dataset[0].image_paths[i]))
-            print('')
+
             
-            # Print distance matrix
-            print('Distance matrix')
-            print('         ', end='')
-            for i in range(nrof_images):
-                print('   %1d     ' % i, end='')
-            print('')
-            for i in range(nrof_images):
-                print('%1d  ' % i, end='')
-                for j in range(nrof_images):
-                    dist = np.sqrt(np.sum(np.square(np.subtract(emb[i,:], emb[j,:]))))
-                    print('  %1.4f  ' % dist, end='')
-                print('')'''
-
-            #kmeans me sklearn
-            '''kmeans = KMeans(n_clusters=args.clusters, random_state=33,max_iter=1000000000,n_init =30, init='random',tol=0.00000001).fit(emb)
-            print (kmeans.labels_)
-            print('Images:')
-            for i in range(nrof_images):
-                print('%1d: %s' % (i, dataset[0].image_paths[i]))
-            print('')
-            for i in range(kmeans.n_clusters):
-                print ("omada : %d"% i)
-                for j in range(nrof_images):
-                    if (i==kmeans.labels_[j]):
-                        print (dataset[0].image_paths[j])
-            print (kmeans.inertia_)'''
             #silhouette
-            #silhouette_avg = silhouette_score(emb, kmeans.labels_)
-            #print(silhouette_avg)
             #----------
-            range_n_clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] #tha ithela n dokimasw oso megalwnoun taclusters
-                                            #an xeirotereuoun ta apotelesmata n stamataeiautomata
-            j=0
-            cluster_labels = [None] * len(range_n_clusters)
-            silhouette_avg = [None] * len(range_n_clusters)
-            for n_clusters in range_n_clusters:
-                # Create a subplot with 1 row and 2 columns
-                fig, (ax1, ax2) = plt.subplots(1, 2)
-                fig.set_size_inches(18, 7)
-
-                # The 1st subplot is the silhouette plot
-                ax1.set_xlim([-1, 1])
-                # The (n_clusters+1)*10 is for inserting blank space between silhouette
-                # plots of individual clusters, to demarcate them clearly.
-                ax1.set_ylim([0, len(emb) + (n_clusters + 1) * 10])
-
-                # Initialize the clusterer with n_clusters value and a random generator
-                # seed of 10 for reproducibility.
-                clusterer = KMeans(n_clusters=n_clusters, random_state=10)
-                cluster_labels[j] = clusterer.fit_predict(emb)
-
-                # The silhouette_score gives the average value for all the samples.
-                # This gives a perspective into the density and separation of the formed
-                # clusters
-                silhouette_avg[j] = silhouette_score(emb, cluster_labels[j])
-                print("For n_clusters =", n_clusters,
-                      "The average silhouette_score is :", silhouette_avg[j])
-
-                # Compute the silhouette scores for each sample
-                sample_silhouette_values = silhouette_samples(emb, cluster_labels[j])
-
-                y_lower = 10
-                for i in range(n_clusters):
-                    # Aggregate the silhouette scores for samples belonging to
-                    # cluster i, and sort them
-                    ith_cluster_silhouette_values = \
-                        sample_silhouette_values[cluster_labels[j] == i]
-
-                    ith_cluster_silhouette_values.sort()
-
-                    size_cluster_i = ith_cluster_silhouette_values.shape[0]
-                    y_upper = y_lower + size_cluster_i
-
-                    color = cm.spectral(float(i) / n_clusters)
-                    ax1.fill_betweenx(np.arange(y_lower, y_upper),
-                                      0, ith_cluster_silhouette_values,
-                                      facecolor=color, edgecolor=color, alpha=0.7)
-
-                    # Label the silhouette plots with their cluster numbers at the middle
-                    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-
-                    # Compute the new y_lower for next plot
-                    y_lower = y_upper + 10  # 10 for the 0 samples
-
-                ax1.set_title("The silhouette plot for the various clusters.")
-                ax1.set_xlabel("The silhouette coefficient values")
-                ax1.set_ylabel("Cluster label")
-
-                # The vertical line for average silhouette score of all the values
-                ax1.axvline(x=silhouette_avg[j], color="red", linestyle="--")
-
-                ax1.set_yticks([])  # Clear the yaxis labels / ticks
-                ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
-
-                # 2nd Plot showing the actual clusters formed
-                colors = cm.spectral(cluster_labels[j].astype(float) / n_clusters)
-                ax2.scatter(emb[:, 0], emb[:, 1], marker='.', s=30, lw=0, alpha=0.7,
-                            c=colors, edgecolor='k')
-
-                # Labeling the clusters
-                centers = clusterer.cluster_centers_
-                # Draw white circles at cluster centers
-                ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
-                            c="white", alpha=1, s=200, edgecolor='k')
-
-                for i, c in enumerate(centers):
-                    ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
-                                s=50, edgecolor='k')
-
-                ax2.set_title("The visualization of the clustered data.")
-                ax2.set_xlabel("Feature space for the 1st embedding")
-                ax2.set_ylabel("Feature space for the 2nd embedding")
-
-                plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
-                              "with n_clusters = %d" % n_clusters),
-                             fontsize=14, fontweight='bold')
-
-                #plt.show()
-                j+=1
-            #best cluster
-            best_cl=silhouette_avg.index(max(silhouette_avg))
-            print('best number of clusters: ',best_cl+2)
-
+            cluster_labels, silhouette_avg, sample_silhouette_values, best_cl = kMSilhouette(emb)
+            
+            #outliers(cluster_labels[best_cl], best_cl+2, sample_silhouette_values[best_cl], silhouette_avg[best_cl], args.outlierConstant)
+            
+            #den einai veltisto!!!
+            output_dir_cluster = [None] * (best_cl+2)
             for i in range(best_cl+2):
-                output_dir_cluster = os.path.expanduser(args.output_dir + '/omada '+str(i))
-                if not os.path.exists(output_dir_cluster):
-                    os.makedirs(output_dir_cluster)
-                if not os.path.exists(output_dir_cluster+' (cropped)'):
-                    os.makedirs(output_dir_cluster+' (cropped)')
-                #print ("omada : %d"% i)
-                for j in range(nrof_images): #twra me voithaei an uparxei to dir na petaei error meta omws th t valw tab mesa sto if not
+                output_dir_cluster[i] = os.path.expanduser(args.output_dir + '/omada '+str(i))
+                if not os.path.exists(output_dir_cluster[i]):
+                    os.makedirs(output_dir_cluster[i])
+                if not os.path.exists(output_dir_cluster[i]+' (cropped)'):
+                    os.makedirs(output_dir_cluster[i]+' (cropped)')
+            
+            for j in range(nrof_images):
+                r,g,b = cv2.split(images[j])
+                img2 = cv2.merge([b*255,g*255,r*255])
+                #path manipulation for imwrite
+                outImWr=output_dir_cluster[cluster_labels[best_cl][j]]+' (cropped)'+'/'+os.path.basename(dataset[0].image_paths[j])
+                cv2.imwrite(outImWr,img2)
+                copy(dataset[0].image_paths[j],output_dir_cluster[cluster_labels[best_cl][j]])
+                '''for j in range(nrof_images): #twra me voithaei an uparxei to dir na petaei error meta omws th t valw tab mesa sto if not
                     if (i==cluster_labels[best_cl][j]):
                         r,g,b = cv2.split(images[j])
                         img2 = cv2.merge([b*255,g*255,r*255])
                         #path manipulation for imwrite
                         outImWr=output_dir_cluster+' (cropped)'+'/'+os.path.basename(dataset[0].image_paths[j])
                         cv2.imwrite(outImWr,img2)
-                        copy(dataset[0].image_paths[j],output_dir_cluster)
+                        copy(dataset[0].image_paths[j],output_dir_cluster)'''
+            print(sample_silhouette_values[2])
+            print('\n')
+            print(cluster_labels[best_cl])
 
 
 
-def frameGetter(vid,output_dir):
+def frame_getter(vid,output_dir):
     frame_interval = 300  # Number of frames after which to save
     frame_rate = 0
     frame_count = 0
@@ -231,6 +122,39 @@ def frameGetter(vid,output_dir):
     # When everything is done, release the capture
     cap.release()
 
+def kMSilhouette(emb):
+    range_n_clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] #tha ithela n dokimasw oso megalwnoun taclusters
+                                            #an xeirotereuoun ta apotelesmata n stamataeiautomata
+    j=0
+    cluster_labels = [None] * len(range_n_clusters)
+    silhouette_avg = [None] * len(range_n_clusters)
+    sample_silhouette_values = [None] * len(range_n_clusters)
+    for n_clusters in range_n_clusters:
+        # Initialize the clusterer with n_clusters value and a random generator
+        # seed of 10 for reproducibility.
+        clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+        cluster_labels[j] = clusterer.fit_predict(emb)
+
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg[j] = silhouette_score(emb, cluster_labels[j])
+        print("For n_clusters =", n_clusters,
+              "The average silhouette_score is :", silhouette_avg[j])
+
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values[j] = silhouette_samples(emb, cluster_labels[j])
+        j+=1
+    #best cluster
+    best_cl=silhouette_avg.index(max(silhouette_avg))
+    print('best number of clusters: ',best_cl+2)
+    return cluster_labels, silhouette_avg, sample_silhouette_values, best_cl
+    
+def outliers(cluster_labels, max_clusters, sample_silhouette_values, silhouette_avg, N):
+    for i in range(max_clusters):
+        if sample_silhouette_values[i] < N :
+            return
+    return
 
 def load_and_align_data(image_paths, image_size, margin, gpu_memory_fraction):
 
@@ -285,7 +209,7 @@ def parse_arguments(argv):
     parser.add_argument('model', type=str, 
         help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf (.pb) file')
     parser.add_argument('output_dir', type=str, help='output directory')
-    parser.add_argument('clusters', type=int, help='number of faces')
+    parser.add_argument('outlierConstant', type=float, help='Constant for fixing outliers')
     parser.add_argument('--image_size', type=int,
         help='Image size (height, width) in pixels.', default=160)
     parser.add_argument('--margin', type=int,
@@ -301,7 +225,7 @@ if __name__ == '__main__':
 #me k-means (mathisi xwris epivlepsi dld den exw etiketes) sta emb na lew poses omades thelw kai na kanei clustering
 
 #de xreiazonte ta plots
-#interface, datatabase me polla video
+#interface, database me polla video
 
 #gia to detect_face:davidsandberg:
 #There are five landmarks detected by MTCNN and these are left eye, right eye, nose, left mouth corner, and right mouth corner.
